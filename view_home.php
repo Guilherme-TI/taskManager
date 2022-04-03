c185c7e2e9ec253e01e1f4095b93910d3c1935ae
Luiz Guilherme <90113778+Guilherme-TI@users.noreply.github.com>
2022-04-03 09:44:15 -0300

    Update view_home.php

--- view_home.php
+++ view_home.php
@@ -11,7 +11,6 @@ require_once "class/Tarefa.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/xicara-de-cafe.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/style.css">
     <title>Task Manager - <?= $_SESSION['nome']; ?></title>
 </head>
